0a2293a9b3ded4fbc5d1f5d0a04b75618b943c96
snehal <Admin@DESKTOP-QUVO9UC>
2024-05-02 11:42:58 +0530

    First commit

---
+++ src/JavaProgram/GitProgram2.java
@@ -0,0 +1,10 @@
+package JavaProgram;
+
+public class GitProgram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("java program 2");
+	}
+
+}
---
+++ src/JavaProgram/gitprogram.java
@@ -0,0 +1,11 @@
+package JavaProgram;
+
+public class gitprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
